5ffefd36056115be7c712be0f761017df5d14947
OliverBrik <242152952+OliverBrik@users.noreply.github.com>
2026-09-22 10:11:25 +0200

    Remove unnessesary files

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="" width="125" height="125" />
-
+   
     <div class="wrapper">
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
